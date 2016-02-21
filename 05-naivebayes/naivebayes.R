978992cc862234bd77ab5fb9910962bd112148bc
Verena Haunschmid <veroamilbe@gmail.com>
2016-02-21 12:01:30 +0100

    update target column

--- 05-naivebayes/naivebayes.R
+++ 05-naivebayes/naivebayes.R
@@ -6,7 +6,7 @@ mush_test<-sample(1:nrow(mush_data), nrow(mush_data)*0.1)
 
 ## naive bayes ##
 library(e1071)
-nb_mush <- naiveBayes(mush_data[-mush_test, paste0("V",1:22)], mush_data[-mush_test, "V23"])
-pred_mush <- predict(nb_mush, mush_data[mush_test, paste0("V",1:22)])
-sum(pred_mush==mush_data[mush_test, "V23"])
+nb_mush <- naiveBayes(mush_data[-mush_test, paste0("V",2:23)], mush_data[-mush_test, "V1"])
+pred_mush <- predict(nb_mush, mush_data[mush_test, paste0("V",2:23)])
+sum(pred_mush==mush_data[mush_test, "V1"])
 
