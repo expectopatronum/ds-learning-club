a36405529e559a766b63d634b52c39052ae9355c
Verena Haunschmid <veroamilbe@gmail.com>
2016-03-05 16:06:15 +0100

    add histogram for task 2

--- 05-naivebayes/naivebayes.R
+++ 05-naivebayes/naivebayes.R
@@ -4,9 +4,13 @@ mush_data<-read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/m
 
 mush_test<-sample(1:nrow(mush_data), nrow(mush_data)*0.1)
 
-## naive bayes ##
+#### naive bayes ####
 library(e1071)
 nb_mush <- naiveBayes(mush_data[-mush_test, paste0("V",2:23)], mush_data[-mush_test, "V1"])
 pred_mush <- predict(nb_mush, mush_data[mush_test, paste0("V",2:23)])
 sum(pred_mush==mush_data[mush_test, "V1"])
 
+#### build AUC ####
+
+pred_train <- predict(nb_mush, mush_data[-mush_test, paste0("V",2:23)])
+#calcAUC
