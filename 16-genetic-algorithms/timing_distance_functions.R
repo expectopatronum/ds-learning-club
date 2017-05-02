e908e18c7d3955457eeec6b042e3e54e6f382c70
expectopatronum <veroamilbe@gmail.com>
2017-05-02 21:21:01 +0200

    uses microbenchmark for timing the functions

--- 16-genetic-algorithms/timing_distance_functions.R
+++ 16-genetic-algorithms/timing_distance_functions.R
@@ -1,7 +1,16 @@
+### packages and other sources
+
+library(ggplot2)
+library(microbenchmark)
+source("genetic_algorithm.R")
+source("data_store_helper.R")
+source("list_helper.R")
+
 ### prep data
 
-population <- generate_random_population(all_waypoints_us, 100)
 data_store_us <- read_tsv("my-waypoints-dist-dur.tsv" )
+all_waypoints_us <- union(data_store_us$waypoint1, data_store_us$waypoint2)
+population <- generate_random_population(all_waypoints_us, 100)
 dist_matrix <- build_distance_matrix(data_store_us)
 
 ### run on data frame
@@ -30,3 +39,28 @@ system.time({
       population_fitness[[length(population_fitness)+1]] <- agent_fitness
     }
   }})
+
+#    user  system elapsed 
+# 0.042   0.000   0.044 
+
+### microbenchmark
+
+res <- microbenchmark({
+  population_fitness <- list()
+  for (agent_genome in population) {
+    if (!contains(population_fitness, agent_genome)) {
+      fit <- compute_fitness_fast(dist_matrix, agent_genome)
+      agent_fitness <- list("agent"=agent_genome, "fitness"=unname(fit))
+      population_fitness[[length(population_fitness)+1]] <- agent_fitness
+    }
+  }},
+  {
+    population_fitness <- list()
+    for (agent_genome in population) {
+      if (!contains(population_fitness, agent_genome)) {
+        fit <- compute_fitness(data_store_us, agent_genome)
+        agent_fitness <- list("agent"=agent_genome, "fitness"=unname(fit))
+        population_fitness[[length(population_fitness)+1]] <- agent_fitness
+      }
+    }},
+  times = 100)
